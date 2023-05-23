2ba42b2af692089309bfd0bf36192b3309db1bd3
Casillas0905 <ikersanmartinbarreiro@gmail.com>
2023-05-23 19:22:25 +0200

    UserInEventController ready

--- Server/src/main/java/com/ServerSep3/Server/Service/MatchService.java
+++ Server/src/main/java/com/ServerSep3/Server/Service/MatchService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface MatchService {
     MatchModel findById(int id);
     MatchModel updateMatch(MatchModel matchModel);
-    void deleteMatch(MatchModel matchModel);
+    void deleteMatch(long id);
     List<MatchModel> findByUserId(int id);
     boolean isMatch(MatchModel matchModel);
     void CreateMatchForUser(UserModel userModel);
